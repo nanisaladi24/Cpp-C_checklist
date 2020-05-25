00847ea2a7a10b934ce990b9898a043b93eeb42e
nanisaladi24 <nanisaladi24@gmail.com>
2020-05-24 17:31:33 -0700

    Multiple problem statements defined for some OOP concepts

---
+++ OOP/class_polymorphism.cpp
@@ -0,0 +1,11 @@
+/*
+Problem:
+- Understand constructors and destructors of class and how they subclasses inherit them
+- Understand 'final' keyword which doesn't allow subclasses to override methods
+- understand purpose of 'virtual' keyword
+- understand overload and overriding
+- struct (also supports functions) vs class. struct cannot support concepts like functional classes(forgot term, but I meant these classes are used to encapsulate a class of functions and variables
+but no object is actually created with this class(mostly static definitions)),inheritance
+*/
+
+// CODE TBD
